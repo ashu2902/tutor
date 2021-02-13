0dca9d8aa555eef7da8ea1bf075ed4616b16edce
ash10 <sharmaashu.2902@gmail.com>
2021-02-13 05:58:30 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Dimensions,StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+class App extends Component {
+  render (){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      
+        <Text style={{fontSize:40,}}>Register as a </Text>
+        <Text style={{fontSize:45,color:'royalblue'}}>Student</Text>
+        <TouchableOpacity style={{backgroundColor:'dodgerblue',width:wp('50%'),height:hp('6.5%'),marginTop:55,borderRadius:20,justifyContent:'center',alignItems:'center'}}><Text>Next</Text></TouchableOpacity>
+        <TouchableOpacity style={{top:hp('37%'),height:hp('8%'),width:wp('25%'),alignItems:'center',justifyContent:'center',backgroundColor:'royalblue',borderTopEndRadius:50,borderTopStartRadius:50}}><Ionicons style={{}} name="chevron-up-outline" size={32}  /></TouchableOpacity>  
+      
+
     </View>
   );
-}
+} }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
+export default App;
